bc275403c7785e22d424e68fd6530111b2e8b399
Validark <NarrevRBX@gmail.com>
2018-04-19 13:44:40 -0600

    Update Enumeration.lua
    - Standardize __namecall to give `GetEnumerationItems()` to Enumerators
    - Optimize `GetEnumerations()` and `GetEnumerationItems()`
    - Memoize `GetEnumerations()` by arranging an array in alphabetical order as `Enumerators` are declared
    - Error on empty `Enumeration` constructions

--- Enumeration.lua
+++ Enumeration.lua
@@ -2,6 +2,7 @@
 -- @author Validark
 
 local Enumerations = {}
+local EnumerationsArray = {}
 
 local function ReadOnlyNewIndex(_, Index, _)
 	error("[Enumeration] Cannot write to index \"" .. tostring(Index) .. "\"", 2)
@@ -15,12 +16,35 @@ local function ReadOnlyIndex(Table)
 	end
 end
 
+local function GetEnumerationsNameCall(Table, MethodName)
+	return function(_, ...)
+		if select(select("#", ...), ...) == MethodName then
+			-- Returns a copy of array Table
+
+			local Copy = {}
+
+			for i = 1, #Table do
+				Copy[i] = Table[i]
+			end
+
+			return Copy
+		else
+			error("[Enumeration] The only valid method of this object is \"" .. MethodName .. "\"", 2)
+		end
+	end
+end
+
 local function IsValidArray(Table)
 	-- @returns bool Whether table Table it is a valid array of type {"Type1", "Type2", "Type3"}
 
 	local Count = 0
 	local HighestIndex = 0
 	local Unwarned = true
+
+	if not next(Table) then
+		warn("[Enumeration] Table EnumTypes is empty")
+		Unwarned = false
+	end
 
 	for i, v in next, Table do
 		if type(i) ~= "number" or 0 >= i then
@@ -46,6 +70,8 @@ local function MakeEnumeration(_, EnumType, EnumTypes)
 	if not IsValidArray(EnumTypes) then error("[Enumeration] Expected table to be an array of the form {\"Type1\", \"Type2\", \"Type3\"}", 2) end
 	if Enumerations[EnumType] then error("[Enumeration] Enumeration of EnumType " .. tostring(EnumType) .. " already exists", 2) end
 
+	local EnumContainer = {}
+
 	for i = 1, #EnumTypes do
 		local Name = EnumTypes[i]
 		local Item = newproxy(true)
@@ -58,17 +84,30 @@ local function MakeEnumeration(_, EnumType, EnumTypes)
 		ItemMetatable.__newindex = ReadOnlyNewIndex
 		ItemMetatable.__metatable = ReadOnlyMetatable
 		ItemMetatable.__tostring = function() return "Enumeration." .. EnumType .. "." .. Name end
-
-		EnumTypes[i] = nil
-		EnumTypes[Name] = Item
+		EnumContainer[Name] = Item
 	end
 
 	local Enumerator = newproxy(true)
 	local EnumeratorMetatable = getmetatable(Enumerator)
-	EnumeratorMetatable.__index = ReadOnlyIndex(EnumTypes)
+	EnumeratorMetatable.__index = ReadOnlyIndex(EnumContainer)
 	EnumeratorMetatable.__newindex = ReadOnlyNewIndex
 	EnumeratorMetatable.__metatable = ReadOnlyMetatable
 	EnumeratorMetatable.__tostring = function() return EnumType end
+	EnumeratorMetatable.__namecall = GetEnumerationsNameCall(EnumTypes, "GetEnumerationItems")
+
+	local InsertedIntoEnumerationsArray -- Place into ordered EnumerationsArray so we don't have to do table.sort upon every GetEnumerations()
+
+	for i = 1, #EnumerationsArray do
+		if EnumType < tostring(EnumerationsArray[i]) then -- Determine whether `key` precedes `EnumerationsArray[i]` alphabetically
+			InsertedIntoEnumerationsArray = true
+			table.insert(EnumerationsArray, i, Enumerator)
+			break
+		end
+	end
+
+	if not InsertedIntoEnumerationsArray then
+		table.insert(EnumerationsArray, Enumerator)
+	end
 
 	Enumerations[EnumType] = Enumerator
 end
@@ -79,21 +118,6 @@ EnumerationMetatable.__index = ReadOnlyIndex(Enumerations)
 EnumerationMetatable.__newindex = MakeEnumeration
 EnumerationMetatable.__metatable = ReadOnlyMetatable
 EnumerationMetatable.__tostring = function() return "Enumerations" end
-EnumerationMetatable.__namecall = function(_, ...)
-	if select(select("#", ...), ...) == "GetEnumerations" then
-		-- Returns an array of all the Enumerations ever created
-		local t = {}
-		local Count = 0
-
-		for _, Enumerator in next, Enumerations do
-			Count = Count + 1
-			t[Count] = Enumerator
-		end
-
-		return t
-	else
-		error("[Enumeration] The only method of Enumeration is \"GetEnumerations\"", 2)
-	end
-end
+EnumerationMetatable.__namecall = GetEnumerationsNameCall(EnumerationsArray, "GetEnumerations")
 
 return Enumeration
