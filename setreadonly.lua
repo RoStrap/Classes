f8d30edd8be91fcbdfd6c5ee9cfdcc861ba0974d
Validark <NarrevRBX@gmail.com>
2018-04-24 11:30:20 -0600

    Update setreadonly.lua

--- setreadonly.lua
+++ setreadonly.lua
@@ -1,8 +1,10 @@
 -- Simple and light-weight function for setting read-only properties of a Class
 
 local function setreadonly(Class, ReadOnly)
+	-- Sets properties within ReadOnly to be accessible but unwritable to tables with metatable set to Class
 	-- @param table Class the Class which should be able to access read-only properties but not write to them
 	-- @param table Readonly the table full of read-only properties to copy into __index and disallow writing
+	-- assumes Class:__newindex is otherwise unused
 
 	local __index = assert(Class.__index, "Class must have __index metamethod before calling setreadonly")
 
